262aebf5136ca6bf0cbf28b35af32f96174b090f
Kasper Sebb' brandt <sebb@sebb.dk>
2018-11-13 18:50:38 +0100

    style fixes

--- src/m/pages/index.js
+++ src/m/pages/index.js
@@ -2,7 +2,7 @@
 * @Author: Kasper Sebb' brandt
 * @Date:   2018-10-23 19:54:04
 * @Last Modified by:   Kasper Sebb' brandt
-* @Last Modified time: 2018-11-12 21:14:53
+* @Last Modified time: 2018-11-13 18:47:20
 */
 import React, { Component } from 'react';
 import ReactMarkdown from 'react-markdown';
@@ -37,7 +37,7 @@ export class Index extends Component {
     return (
       <div>
         <div className="v-section vcol vsmrow">
-          <div>
+          <div class="vsmlcol">
             <div className="vsmpar40">
               <div className="title-section">
                 <h1>Sebastian Vargr</h1>
@@ -131,97 +131,100 @@ export class Index extends Component {
           </div>
         </div>
 
-        <div className="v-section v-section-padded bggrey">
-          <p className="section-title">
-            <b>Languages</b><br />
-            <small>The human ones. :)</small>
-          </p>
-          <ul className="skills-section">
-            <li className="v-skill v-col-sm-6"><span>Danish</span></li>
-            <li className="v-skill v-col-sm-6"><span>English</span></li>
-          </ul>
+        <div className="v-section vcol vsmrow bggrey">
+          <div class="vsmlcol"></div>
+          <div>
+            <p className="section-title">
+              <b>Languages</b><br />
+              <small>The human ones. :)</small>
+            </p>
+            <ul className="skills-section">
+              <li className="v-skill v-col-sm-6"><span>Danish</span></li>
+              <li className="v-skill v-col-sm-6"><span>English</span></li>
+            </ul>
 
 
-          <p className="section-title">
-            <b>Programming languages & variations</b><br />
-            <small>Languages and variants i have worked with in large or lesser extend, i know others but these are the ones i usually work in.</small>
-          </p>
-          <ul className="skills-section">
-            <li className="v-skill"><span>Javascript / ES6+</span></li>
-            <li className="v-skill"><span>Typescript</span></li>
-            <li className="v-skill"><span>Actionscript 3</span></li>
-            <li className="v-skill"><span>PHP</span></li>
-            <li className="v-skill"><span>C#</span></li>
-            <li className="v-skill"><span>Java</span></li>
-            <li className="v-skill"><span>HTML5</span></li>
-            <li className="v-skill"><span>CSS3</span></li>
-            <li className="v-skill"><span>Sass</span></li>
-            <li className="v-skill"><span>LESS</span></li>
-            <li className="v-skill"><span>PostCSS</span></li>
-            <li className="v-skill"><span>JSON</span></li>
-            <li className="v-skill"><span>XML</span></li>
-          </ul>
- 
-          <p className="section-title">
-            <b>Tools & tecknologies</b><br />
-            <small>An outake of some of the libraries, tools and patterns i have used.</small>
-          </p>
-          <ul className="skills-section">
-            <li className="v-skill"><span>Git</span></li>
-            <li className="v-skill"><span>NodeJS</span></li>
-            <li className="v-skill"><span>Unity3D</span></li>
-            <li className="v-skill"><span>Flash CS</span></li>
-            <li className="v-skill"><span>Photoshop</span></li>
-            <li className="v-skill"><span>MySQL</span></li>
-            <li className="v-skill"><span>MongoDB</span></li>
-            <li className="v-skill"><span>Bower</span></li>
-            <li className="v-skill"><span>Gulp</span></li>
-            <li className="v-skill"><span>Webpack</span></li>
-            <li className="v-skill"><span>Babel</span></li>
-            <li className="v-skill"><span>Angular</span></li>
-            <li className="v-skill"><span>AngularJS</span></li>
-            <li className="v-skill"><span>React</span></li>
-            <li className="v-skill"><span>Cordova (Phonegab)</span></li>
-            <li className="v-skill"><span>Jest</span></li>
-            <li className="v-skill"><span>Karma</span></li>
-            <li className="v-skill"><span>Jasmine</span></li>
-            <li className="v-skill"><span>Protractor</span></li>
-            <li className="v-skill"><span>jQuery</span></li>
-            <li className="v-skill"><span>Backbone.js</span></li>
-            <li className="v-skill"><span>Underscore.js</span></li>
-            <li className="v-skill"><span>PhantomJS</span></li>
-            <li className="v-skill"><span>Ionic framework</span></li>
-            <li className="v-skill"><span>Wordpress</span></li>
-            <li className="v-skill"><span>Redux</span></li>
-            <li className="v-skill"><span>Flux</span></li>
-          </ul>
+            <p className="section-title">
+              <b>Programming languages & variations</b><br />
+              <small>Languages and variants i have worked with in large or lesser extend, i know others but these are the ones i usually work in.</small>
+            </p>
+            <ul className="skills-section">
+              <li className="v-skill"><span>Javascript / ES6+</span></li>
+              <li className="v-skill"><span>Typescript</span></li>
+              <li className="v-skill"><span>Actionscript 3</span></li>
+              <li className="v-skill"><span>PHP</span></li>
+              <li className="v-skill"><span>C#</span></li>
+              <li className="v-skill"><span>Java</span></li>
+              <li className="v-skill"><span>HTML5</span></li>
+              <li className="v-skill"><span>CSS3</span></li>
+              <li className="v-skill"><span>Sass</span></li>
+              <li className="v-skill"><span>LESS</span></li>
+              <li className="v-skill"><span>PostCSS</span></li>
+              <li className="v-skill"><span>JSON</span></li>
+              <li className="v-skill"><span>XML</span></li>
+            </ul>
 
-          <p className="section-title">
-            <b>Industry and other generel Knowhow</b><br />
-            <small>These concepts are also worth mentioning.</small>
-          </p>
-          <ul className="skills-section">
-            <li className="v-skill"><span>SEO</span></li>
-            <li className="v-skill"><span>Single page applications</span></li>
-            <li className="v-skill"><span>Backend developement</span></li>
-            <li className="v-skill"><span>Frontend developement</span></li>
-            <li className="v-skill"><span>Responsive layouts</span></li>
-            <li className="v-skill"><span>Graceful degradation</span></li>
-            <li className="v-skill"><span>Continuous integration</span></li>
-            <li className="v-skill"><span>E2E testing</span></li>
-            <li className="v-skill"><span>Unit testing</span></li>
-            <li className="v-skill"><span>RESTfull API</span></li>
-            <li className="v-skill"><span>Websockets</span></li>
-            <li className="v-skill"><span>Scrum</span></li>
-            <li className="v-skill"><span>Agile</span></li>
-            <li className="v-skill"><span>Project management</span></li>
-            <li className="v-skill"><span>Game design</span></li>
-            <li className="v-skill"><span>Internationalization</span></li>
-            <li className="v-skill"><span>Push notification android and iOS</span></li>
-            <li className="v-skill"><span>Cross platform development</span></li>
-            <li className="v-skill"><span>Linux / Debian system administration</span></li>
-            <li className="v-skill"><span>Data security</span></li>
-          </ul>
+            <p className="section-title">
+              <b>Tools & tecknologies</b><br />
+              <small>An outake of some of the libraries, tools and patterns i have used.</small>
+            </p>
+            <ul className="skills-section">
+              <li className="v-skill"><span>Git</span></li>
+              <li className="v-skill"><span>NodeJS</span></li>
+              <li className="v-skill"><span>Unity3D</span></li>
+              <li className="v-skill"><span>Flash CS</span></li>
+              <li className="v-skill"><span>Photoshop</span></li>
+              <li className="v-skill"><span>MySQL</span></li>
+              <li className="v-skill"><span>MongoDB</span></li>
+              <li className="v-skill"><span>Bower</span></li>
+              <li className="v-skill"><span>Gulp</span></li>
+              <li className="v-skill"><span>Webpack</span></li>
+              <li className="v-skill"><span>Babel</span></li>
+              <li className="v-skill"><span>Angular</span></li>
+              <li className="v-skill"><span>AngularJS</span></li>
+              <li className="v-skill"><span>React</span></li>
+              <li className="v-skill"><span>Cordova (Phonegab)</span></li>
+              <li className="v-skill"><span>Jest</span></li>
+              <li className="v-skill"><span>Karma</span></li>
+              <li className="v-skill"><span>Jasmine</span></li>
+              <li className="v-skill"><span>Protractor</span></li>
+              <li className="v-skill"><span>jQuery</span></li>
+              <li className="v-skill"><span>Backbone.js</span></li>
+              <li className="v-skill"><span>Underscore.js</span></li>
+              <li className="v-skill"><span>PhantomJS</span></li>
+              <li className="v-skill"><span>Ionic framework</span></li>
+              <li className="v-skill"><span>Wordpress</span></li>
+              <li className="v-skill"><span>Redux</span></li>
+              <li className="v-skill"><span>Flux</span></li>
+            </ul>
+
+            <p className="section-title">
+              <b>Industry and other generel Knowhow</b><br />
+              <small>These concepts are also worth mentioning.</small>
+            </p>
+            <ul className="skills-section">
+              <li className="v-skill"><span>SEO</span></li>
+              <li className="v-skill"><span>Single page applications</span></li>
+              <li className="v-skill"><span>Backend developement</span></li>
+              <li className="v-skill"><span>Frontend developement</span></li>
+              <li className="v-skill"><span>Responsive layouts</span></li>
+              <li className="v-skill"><span>Graceful degradation</span></li>
+              <li className="v-skill"><span>Continuous integration</span></li>
+              <li className="v-skill"><span>E2E testing</span></li>
+              <li className="v-skill"><span>Unit testing</span></li>
+              <li className="v-skill"><span>RESTfull API</span></li>
+              <li className="v-skill"><span>Websockets</span></li>
+              <li className="v-skill"><span>Scrum</span></li>
+              <li className="v-skill"><span>Agile</span></li>
+              <li className="v-skill"><span>Project management</span></li>
+              <li className="v-skill"><span>Game design</span></li>
+              <li className="v-skill"><span>Internationalization</span></li>
+              <li className="v-skill"><span>Push notification android and iOS</span></li>
+              <li className="v-skill"><span>Cross platform development</span></li>
+              <li className="v-skill"><span>Linux / Debian system administration</span></li>
+              <li className="v-skill"><span>Data security</span></li>
+            </ul>
+          </div>
         </div>
 
         <div className="v-section v-section-padded print-hide hide">
