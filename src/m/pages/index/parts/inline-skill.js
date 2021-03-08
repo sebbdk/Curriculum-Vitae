0a80f43f334fcbeb1baa30612676dd8af7498746
sebbdk <sebbdk@users.noreply.github.com>
2021-03-08 23:22:48 +0000

    deploy: a6d20264d196d1cabd5d83c3b6b6e09db8908062

--- src/m/pages/index/parts/inline-skill.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class extends Component {
-
-  render() {
-    return <span className="v-skill v-skill--inline"><span>{this.props.children}</span></span>
-  }
-
-}
